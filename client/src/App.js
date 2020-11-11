26426675acba31a552947fd4fed7e31ef4eb5f4c
Victoria Mount <victoriamount01@gmail.com>
2020-11-11 17:29:21 -0600

    Complete parts 1 and 2 of MVP

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
+import UpdateForm from './Movies/UpdateForm'
 import Movie from "./Movies/Movie";
 import axios from 'axios';
 
@@ -22,7 +23,7 @@ const App = () => {
 
   useEffect(() => {
     getMovieList();
-  }, []);
+  }, [movieList]);
 
   return (
     <>
@@ -34,6 +35,10 @@ const App = () => {
 
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
+      </Route>
+
+      <Route path='/update-movie/:id'>
+        <UpdateForm movieList={movieList} setMovieList={setMovieList} />
       </Route>
     </>
   );
